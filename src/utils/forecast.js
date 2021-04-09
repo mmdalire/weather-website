2468b3764e1543d5718c7d9af785401618bdb4ce
Mark Dalire <mmdalire@gmail.com>
2021-04-09 17:31:26 +0800

    Add details for humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,26 @@
-const request = require('request');
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=9366e20c9d1d5910f007acd904c09503&query=${latitude},${longitude}&units=f`;
-    
-    request({ url, json: true }, (error, { body } = {}) => {
-        if(error) {
-            callback('Unable to connect to weather service!', undefined);
-        } else if (body.error) { 
-            callback('Unable to find location!', undefined);
-        } else {
-            const {temperature, feelslike, weather_descriptions} = body.current;
-            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`);
-        }
-    });
-}
+  const url = `http://api.weatherstack.com/current?access_key=9366e20c9d1d5910f007acd904c09503&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location!", undefined);
+    } else {
+      const {
+        temperature,
+        feelslike,
+        weather_descriptions,
+        humidity,
+      } = body.current;
+      callback(
+        undefined,
+        `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out. The humidity is ${humidity}%`
+      );
+    }
+  });
+};
 
 module.exports = forecast;
