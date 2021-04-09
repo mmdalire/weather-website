474f4f74a4c351053ed61fca1f879f67abd67c82
Mark Dalire <mmdalire@gmail.com>
2021-04-09 13:48:30 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,25 @@
-const weatherForm = document.querySelector('form');
-const searchElement = document.querySelector('input');
-const messageOne = document.querySelector('#message-1');
-const messageTwo = document.querySelector('#message-2');
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const location = searchElement.value;
-    
-    messageOne.textContent = 'Loading...';
-    messageTwo.textContent = '';
-    
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-        response.json().then(({error, location, forecast} = {}) => {
-            if(error) {
-                messageOne.textContent = error;
-                messageTwo.textContent = '';
-            } else { 
-                messageOne.textContent = location;
-                messageTwo.textContent = forecast;
-            }
-        })
-    })
+  const location = searchElement.value;
 
-})
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then(({ error, location, forecast } = {}) => {
+      if (error) {
+        messageOne.textContent = error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = location;
+        messageTwo.textContent = forecast;
+      }
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,106 +1,105 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('hbs');
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Mark Dalire'
-    });
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Mark Dalire'
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        message: 'This is a sample message',
-        title: 'Help',
-        name: 'Mark Dalire'
-    })
-})
-
-
-app.get('/weather', (req, res) => {
-    const {address} = req.query;
-    if(!address) {
-        return res.send({
-            error: 'You must provide an address!'
-        });
-    }
-
-    geocode(address, (error, {latitude, longitude, location} = {}) => {
-        if(error) {
-            return res.send({ error });
-        } 
-    
-        forecast(latitude, longitude, (error, forecastData) => {
-            if(error) {
-                return res.send({ error });
-            }
-
-            res.send({
-                location,
-                forecast: forecastData,
-                address
-            })
-        })
-    })
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Mark Dalire",
+  });
 });
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        res.send({
-            error: 'You must provide a search term!'
-        });
-        return;
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Mark Dalire",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "This is a sample message",
+    title: "Help",
+    name: "Mark Dalire",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  const { address } = req.query;
+  if (!address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
     }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        location,
+        forecast: forecastData,
+        address,
+      });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
     res.send({
-        products: []
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    // res.send('Help article not found')
-    res.render('404', {
-        title: 404,
-        name: 'Mark Dalire',
-        not_found_message: 'Help article not found!'
+      error: "You must provide a search term!",
     });
-})
+    return;
+  }
+  res.send({
+    products: [],
+  });
+});
 
-app.get('*', (req, res) => {
-    // res.send('My 404 page')
-    res.render('404', {
-        title: 404,
-        name: 'Mark Dalire',
-        not_found_message: 'Page not found!'
-    });
-})
+app.get("/help/*", (req, res) => {
+  // res.send('Help article not found')
+  res.render("404", {
+    title: 404,
+    name: "Mark Dalire",
+    not_found_message: "Help article not found!",
+  });
+});
 
+app.get("*", (req, res) => {
+  // res.send('My 404 page')
+  res.render("404", {
+    title: 404,
+    name: "Mark Dalire",
+    not_found_message: "Page not found!",
+  });
+});
 
-app.listen(3000, () => {
-    console.log(`Server is up on port 3000`);
-})
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
